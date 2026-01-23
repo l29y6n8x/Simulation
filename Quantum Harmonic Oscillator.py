9319efea3a77614c92e5a463b2efe9b40c74d9f9
l29y6n8x <linus.bartholome@gmail.com>
2026-01-23 14:54:54 +0100

    complete hermite polynomials from scipy.special

--- Quantum Harmonic Oscillator.py
+++ Quantum Harmonic Oscillator.py
@@ -1,52 +1,36 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import math 
+import matplotlib.pyplot as plt
+from math import factorial
+from scipy.special import hermite
 
+# Parameter
+n = 15
+m = 9.11e-31
+w = 2 * np.pi
+hbar = 1.054571817e-34
 
-
-n = 2
-m = 9.11 * 10**(-31)
-w = 2*np.pi
-h_bar = 1.054571817e-34
-psi = 0
-
-scale = 10e-3
+scale = 1e-1
 yscale = 200
 
-fig = plt.figure(figsize=(10,10))
-ax = plt.subplot(1,1,1)
-ax.set_xlim(-scale, scale)
-ax.set_ylim(-yscale,yscale)
+# dimensionslose Variable
+def xi(x):
+    return np.sqrt(m * w / hbar) * x
 
-def fakultät(b):
-    result = 1
-    for i in range(b):
-        result *= (i+1)
-    return result
+# Eigenfunktion
+def oscillator(x):
+    Hn = hermite(n)          # physikalisches Hermite-Polynom
+    prefactor = (m*w/(np.pi*hbar))**0.25
+    norm = 1 / np.sqrt(2**n * factorial(n))
+    return prefactor * norm * Hn(xi(x)) * np.exp(-0.5 * xi(x)**2)
 
-def H_func(c,d):
-    if c == 0:
-        return 1
-    if c == 1:
-        return 2 * d/(np.sqrt(h_bar/(m*w)))
-    if c == 2:
-        return (2 * d/(np.sqrt(h_bar/(m*w))))**2 - 2
+# Plot
+x_data = np.linspace(-scale, scale, 10_000)
+y_data = np.abs(oscillator(x_data))**2
 
-def oscillator(a):
-    psi = (m * w / (h_bar * np.pi))**(1/4) * 1/np.sqrt(math.factorial(n) * 2**n) * H_func(n,a) * np.e ** (-1/2 * (m * w / h_bar) * a ** 2)
-    return psi
-
-
-
-        
-
-
-
-def display():
-    ax.plot(x_data, y_data)
-x_data = np.linspace(-scale,scale,10000)
-y_data = (np.absolute(oscillator(x_data))**2)
-
-display()
-
+plt.figure(figsize=(10,10))
+plt.plot(x_data, y_data)
+plt.xlim(-scale, scale)
+plt.ylim(0, yscale)
+plt.xlabel("x")
+plt.ylabel(r"$|\psi_" + str(n) + "(x)|^2$")
 plt.show()
